b64549a08cc30d86c096e4a254951d13f0a56fa3
Amar Maksumić <amarmaksumich@gmail.com>
2021-01-17 14:46:19 -0500

    testing out vue

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <ImageSection img_src="vision_calculations.jpg"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import ImageSection from './components/ImageSection.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    ImageSection,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/ImageSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <section :style="{'background-image':'url(/src/assets/' + `${img_src}` + ')'}">
+    test
+    {{ img_src }}
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ImageSection',
+  props: {
+    img_src: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
